f8c63a349d566e64b3915467c605f74d4e13c196
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-07-27 19:42:46 -0700

    Tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,13 +16,13 @@ def home_page():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_view():
-    """Displays 'HBNB' at the /hbnb route."""
+    """Displays HBNB at the /hbnb route."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
-    """Displays 'C' followed by the value of 'text"""
+    """Displays C followed by the value of text"""
     text = text.replace("_", " ")
     return f'C {escape(text)}'
 
